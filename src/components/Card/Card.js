d75ae376a32c42f439799331cfdceed6ce6486d1
Michał Zagojski <zagojski.michal.99@gmail.com>
2021-07-21 12:13:44 +0200

    Switch application to Redux

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import styles from './Card.scss';
+import PropTypes from 'prop-types';
 
-const Card = props => {
-    return (
-        <div className = {styles.component}>
-            <section>
-                <h3>{props.title}</h3>
-            </section>
-        </div>
-    )
-}
+const Card = ({title}) => {
+  return (
+    <div className = {styles.component}>
+      <section>
+        <h3>{title}</h3>
+      </section>
+    </div>
+  );
+};
+Card.propTypes = {
+  title: PropTypes.node,
+};
 
-export default Card
+export default Card;
--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,43 +1,36 @@
 import React, { Component } from 'react';
 import styles from './Column.scss';
 import Card from '../Card/Card';
-import Creator from '../Creator/Creator';
+import PropTypes from 'prop-types';
+//import Creator from '../Creator/Creator';
 import Icon from '../Icon/Icon';
-import { settings } from '../../data/dataStore';
+//import { settings } from '../../data/dataStore';
 
 export default class Column extends Component {
-    state = {
-        cards: this.props.cards || [],
-      }
-      addCard(title){
-        this.setState(state => (
-          {
-            cards: [
-              ...state.cards,
-              {
-                key: state.cards.length ? state.cards[state.cards.length-1].key+1 : 0,
-                title,
-              }
-            ]
-          }
-        ));
-      }
+    static propTypes = {
+      title: PropTypes.node,
+      cards: PropTypes.array,
+      icon: PropTypes.node,
+      map: PropTypes.func,
+    };
+      
     render() {
-        return (
-            <div className = {styles.component}>
-                <section >
-                    <h3 className = {styles.title}>{this.props.title}
-                    <span className = {styles.icon}>
-                       <Icon name = {this.props.icon}/> 
-                    </span></h3>
-                    {this.state.cards.map(({key, ...cardProps}) => (
-                        <Card key={key} {...cardProps} />
-                    ))}
-                    <div>
+      const {title, cards, icon} = this.props; // eslint-disable-line no-unused-vars
+      return (
+        <div className = {styles.component}>
+          <section >
+            <h3 className = {styles.title}>{this.props.title}
+              <span className = {styles.icon}>
+                <Icon name = {icon}/> 
+              </span></h3>
+            {cards.map(({key, ...cardProps}) => (
+              <Card key={key} {...cardProps} />
+            ))}
+            {/*<div>
                         <Creator text={settings.cardCreatorText} action={title => this.addCard(title)}/>
-                    </div>
-                </section>
-            </div>
-        )
+                    </div>*/}
+          </section>
+        </div>
+      );
     }
 }
